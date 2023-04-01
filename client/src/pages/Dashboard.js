f7ee3c77d0129b95e46506a2d3d1f396df8fa7b5
Marek <marcovindy@gmail.com>
2023-04-01 22:26:11 +0200

    dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -158,38 +158,6 @@ const Dashboard = () => {
           );
         })}
       </div>
-
-      <div class="card" style="max-width: 22rem;">
-
-        <div class="view overlay">
-          <Image class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/food.webp" alt="Card image cap"/>
-            <a>
-              <div class="mask rgba-white-slight waves-effect waves-light"></div>
-            </a>
-        </div>
-
-        <a class="btn-floating btn-action ml-auto mr-4 mdb-color lighten-3 waves-effect waves-light"><i class="fas fa-chevron-right pl-1"></i></a>
-
-        <div class="card-body">
-
-          <h4 class="card-title">Card title</h4>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-              content.</p>
-
-        </div>
-
-        <div class="rounded-bottom mdb-color lighten-3 text-center pt-3">
-          <ul class="list-unstyled list-inline font-small">
-            <li class="list-inline-item pr-2 white-text"><i class="far fa-clock pr-1"></i>05/10/2015</li>
-            <li class="list-inline-item pr-2"><a href="#" class="white-text"><i class="far fa-comments pr-1"></i>12</a></li>
-            <li class="list-inline-item pr-2"><a href="#" class="white-text"><i class="fab fa-facebook-f pr-1">
-            </i>21</a></li>
-            <li class="list-inline-item"><a href="#" class="white-text"><i class="fab fa-twitter pr-1"> </i>5</a></li>
-          </ul>
-        </div>
-
-      </div>
-
     </div>
   );
 }
