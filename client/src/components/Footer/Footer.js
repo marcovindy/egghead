582dbe93bd72dd79d9fc5fe54a0fb94f71dbd975
Marek <marcovindy@gmail.com>
2023-04-13 13:50:04 +0200

    Responsivity of Footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,32 +1,33 @@
 import React from "react";
-import {Row, Col, Button} from "react-bootstrap";
+import { Row, Col, Button, Container } from "react-bootstrap";
 import { Discord, Github } from 'react-bootstrap-icons';
 import Logo from "../../assets/images/trivia.png";
 
 const Footer = () => (
   <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 m-0 border-top">
-    <Row className="justify-content-between container p-2">
-
-      <div className="col-md-4 col-xs-12 d-flex align-items-center">
-        <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-          <img className='hm-50px' src={Logo} />
-        </a>
-        <span className="mb-3 mb-md-0 text-muted">© 2023 Marek Vaníček</span>
-      </div>
-
-      <ul className="nav col-md-4 col-xs-12 justify-content-end list-unstyled d-flex">
-        <li className="nav-item me-3">
-          <a className="nav-link" href="https://discord.gg/eCXJTQ7EXQ" target="_blank" rel="noreferrer">
-            <Discord size={24} />
+    <Container className="p-0">
+      <Row className="justify-content-between container p-2 w-100 mw-100">
+        <div className="col-md-4 col-xs-12 d-flex align-items-center mobile-justify-content-center">
+          <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+            <img className='hm-50px' src={Logo} />
           </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="https://github.com/marcovindy/egghead" target="_blank" rel="noreferrer">
-            <Github size={24} />
-          </a>
-        </li>
-      </ul>
-    </Row>
+          <span className="mb-3 mb-md-0 text-muted">© 2023 Marek Vaníček</span>
+        </div>
+
+        <ul className="nav col-md-4 col-xs-12 justify-content-end list-unstyled d-flex mobile-justify-content-center">
+          <li className="nav-item me-3">
+            <a className="nav-link" href="https://discord.gg/eCXJTQ7EXQ" target="_blank" rel="noreferrer">
+              <Discord size={24} />
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="https://github.com/marcovindy/egghead" target="_blank" rel="noreferrer">
+              <Github size={24} />
+            </a>
+          </li>
+        </ul>
+      </Row>
+    </Container>
   </footer>
 );
 
