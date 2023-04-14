78e1c5f06847431507afe3818f649094e269ab61
Marek <marcovindy@gmail.com>
2023-04-15 01:23:53 +0200

    GamePlayer and EndGame cleaned

--- client/src/components/EndGame/EndGame.js
+++ client/src/components/EndGame/EndGame.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useHistory, Link  } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import { Form, Container, Button, Table } from 'react-bootstrap';
 import './EndGame.css';
+import ListOfPlayers from '../ListOfPlayers/ListOfPlayers';
 
 const EndGame = ({ players, player }) => {
-    let history = useHistory ();
+    let history = useHistory();
 
     const handleSubmit = e => {
         e.preventDefault();
@@ -12,7 +13,7 @@ const EndGame = ({ players, player }) => {
         fetch('http://localhost:5000/scores/save', {
             method: 'POST',
             body: JSON.stringify({
-                username: player.username, 
+                username: player.username,
                 score: player.score
             }),
             headers: {
@@ -20,47 +21,22 @@ const EndGame = ({ players, player }) => {
                 'Accept': 'applicaton/json'
             }
         }).then((response) => {
-            if(response.status === 200) {
+            if (response.status === 200) {
                 console.log('Score has been saved');
                 history.push('/leaderboard');
             };
         });
     };
 
-    return(
+    return (
         <Container>
             <div>
                 <h2>The game has ended!</h2>
                 <div className="score-container">
-                <h3>Game scores</h3>
-                    <Table striped bordered hover>
-                        <thead>
-                            <tr>
-                                <th>Player name</th>
-                                <th>Score</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {players.map((player, index) =>
-                                <tr key={index}>
-                                    <td>{player.username}</td>
-                                    <td>{player.score}</td>
-                                </tr>
-                            )}
-                        </tbody>
-                    </Table>
+                    <h3>Game scores</h3>
+                    <ListOfPlayers playersInRoom={players} />
                 </div>
-
-                {/* <div className="save-score-container">
-                    <h3>Save score to leaderboard</h3>
-                    <Form onSubmit={handleSubmit} method="POST">
-                        <input disabled={true} readOnly defaultValue={player.username} className="form-control"/>
-                        <input disabled={true} readOnly defaultValue={player.score} className="form-control"/>
-                        <Button variant="primary" type="submit">Save score</Button>
-                    </Form>
-                </div> */}
-                <p>Don't leave, if you want to play again!</p>
-                <Link to="/customgame">Leave room</Link>
+                <Link to="/customgame"><Button>Leave room</Button></Link>
             </div>
         </Container>
     );
--- client/src/components/GamePlayer/GamePlayer.js
+++ client/src/components/GamePlayer/GamePlayer.js
@@ -7,68 +7,58 @@ import Messages from '../Messages/Messages';
 import GameQuestion from '../GameQuestion/GameQuestion';
 import ListOfPlayers from '../ListOfPlayers/ListOfPlayers';
 import EndGame from '../EndGame/EndGame';
-import Scoreboard from '../Scoreboard/Scoreboard';
-
-
-import { TransitionGroup, Transition } from 'react-transition-group';
 import './GamePlayer.css';
 
 let socket;
 
-const GamePlayer = ({ location }) => {
+const IS_PROD = process.env.NODE_ENV === 'development';
+const URL = IS_PROD ? 'http://localhost:5000/' : 'https://testing-egg.herokuapp.com/';
 
-    const IS_PROD = process.env.NODE_ENV === "development";
-    const URL = IS_PROD ? "http://localhost:5000/" : "https://testing-egg.herokuapp.com/";
-    const server = URL;
+const GamePlayer = ({ location }) => {
     const [joinRoomName, setJoinRoomName] = useState('');
     const [playerName, setPlayerName] = useState('');
     const [roomPlayer, setRoomPlayer] = useState([]);
-
     const [error, setError] = useState(false);
     const [errorMsg, setErrorMsg] = useState('');
-
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
     const [playersInRoom, setPlayersInRoom] = useState([]);
     const [playerCount, setPlayerCount] = useState([]);
-
     const [gameStart, setGameStart] = useState(false);
     const [currentQuestion, setCurrentQuestion] = useState('');
     const [currentOptions, setCurrentOptions] = useState([]);
     const [currentRound, setCurrentRound] = useState(0);
     const [correctAnswer, setCorrectAnswer] = useState('');
     const [clickActivated, setClickActivated] = useState(true);
-
-    // game end
-    const [players, setPlayers] = useState([]); // to get final score
+    const [players, setPlayers] = useState([]);
     const [gameEnd, setGameEnd] = useState(false);
-    const [player, setPlayer] = useState(''); // to get each client final score
-
+    const [player, setPlayer] = useState('');
     const [playersInfo, setPlayersInfo] = useState([]);
     const [playersScore, setPlayersScore] = useState([]);
     const [score, setScore] = useState([]);
-
     const [timeLeft, setTimeLeft] = useState(0);
     const questionDuration = 20;
     const progress = 100 - ((questionDuration - timeLeft) / questionDuration) * 100;
     const [timerStarted, setTimerStarted] = useState(false);
 
+    const history = useHistory();
+
     useEffect(() => {
         const { joinRoomName, playerName } = queryString.parse(location.search);
-        socket = io.connect(server);
         setJoinRoomName(joinRoomName);
         setPlayerName(playerName);
+        socket = io.connect(URL);
 
         socket.emit('joinRoom', { joinRoomName, playerName }, (error) => {
             if (error) {
                 setError(true);
                 setErrorMsg(error);
                 console.log(error);
-            };
+            }
         });
 
         socket.on('playerData', (allPlayersInRoom) => {
-            setPlayersInRoom(allPlayersInRoom); // is empty the first time, but it is set the next time
+            setPlayersInRoom(allPlayersInRoom);
             setPlayerCount(allPlayersInRoom.length);
         });
 
@@ -76,15 +66,11 @@ const GamePlayer = ({ location }) => {
             socket.emit('disconnect');
             socket.disconnect();
         };
-    }, [server]);
+    }, [location.search]);
 
     useEffect(() => {
-        socket.on('message', (text) => {
-            setMessage(text);
-        });
-        socket.on('message', (message) => {
-            setMessages([...messages, message]);
-        });
+        socket.on('message', (text) => setMessage(text));
+        socket.on('message', (message) => setMessages([...messages, message]));
     }, [messages]);
 
     useEffect(() => {
@@ -97,21 +83,12 @@ const GamePlayer = ({ location }) => {
             setGameEnd(false);
             setClickActivated(true);
         });
-    }, []);
 
-    // set value to false from click function in GameQuestion (onClickChange)
-    const handleClickChange = (val) => {
-        setClickActivated(val);
-    };
-
-    useEffect(() => {
         socket.on('correctAnswer', (correctAnswer) => {
             setCorrectAnswer(correctAnswer);
             console.log('Action 2 correctAnswer');
         });
-    }, []);
 
-    useEffect(() => {
         socket.on('scores', (players) => {
             setPlayers(players);
             setGameEnd(true);
@@ -125,96 +102,91 @@ const GamePlayer = ({ location }) => {
         socket.on('finalPlayerInfo', (client) => {
             setPlayer(client);
         });
-    }, []);
 
-
-    useEffect(() => {
         socket.on('timer', (secs) => {
             const elapsed = questionDuration - secs;
             setTimeLeft(questionDuration - elapsed);
             setTimerStarted(true);
         });
+    }, [questionDuration]);
+
+    const handleLeaveRoom = () => {
+        socket.emit("leaveRoom", playerName);
+    };
+
+    useEffect(() => {
+        socket.on("errorMsg", (err) => {
+            setError(true);
+            setErrorMsg(err);
+        });
+
+        return () => {
+            socket.off("errorMsg");
+        };
     }, []);
 
-
+    const handleClickChange = (val) => {
+        setClickActivated(val);
+    };
 
     return (
         <Container>
-            <div>
-
-            </div>
-            <div className="wrapper">
-                {error === true ? (
-                    <div className="errorMsg">
-                        <p>{errorMsg.error}</p>
-                        <Link to="/customgames">Go back</Link>
+            {error && (
+                <div className="errorMsg">
+                    <p>{errorMsg.error}</p>
+                    <Link to="/customgame">Go back</Link>
+                </div>
+            )}
+            {!gameStart && (
+                <>
+                    {timerStarted ? (
+                        <div>
+                            <h3>Game will be started: {timeLeft}</h3>
+                            <ProgressBar animated now={progress} label={`${timeLeft} seconds left`} />
+                        </div>
+                    ) : (
+                        <div>
+                        <h2>Hello, Game player {playerName}!</h2>
+                        <p><strong>Waiting for Game Master to start the game...</strong></p>
+                        <div className="messages-container">
+                            <h3>Activity</h3>
+                            <hr />
+                            <Messages messages={messages} />
+                        </div>
+                        <a href="/lobby">Leave room</a>
                     </div>
-                ) : (
-                    <div>
-                        {gameStart === false ? (
-                            <>
-                                {timerStarted === true ? (
-                                    <div>
-                                        <h3>Game will be started: {timeLeft}</h3>
-                                        <ProgressBar animated now={progress} label={`${timeLeft} seconds left`} />
-                                    </div>
-                                ) : (
-                                    ""
-                                )}
-                                <div>
-                                    <h2>Hello, Game player {playerName}!</h2>
-                                    <p><strong>Waiting for Game Master to start the game...</strong></p>
-
-                                    <div className="messages-container">
-                                        <h3>Activity</h3>
-                                        <hr />
-                                        <Messages messages={messages} />
-                                    </div>
-                                    <a href="/lobby">Leave room</a>
-                                </div>
-                            </>
-                        ) : (
-                            <div>
-
-
-
-                                {gameEnd === false ? (
-                                    <div>
-                                        <h3>Time left: {timeLeft}</h3>
-                                        <ProgressBar animated now={progress} label={`${timeLeft} seconds left`} />
-                                        <GameQuestion
-                                            currentQuestion={currentQuestion}
-                                            currentOptions={currentOptions}
-                                            currentRound={currentRound}
-                                            playerName={playerName}
-                                            socket={socket}
-                                            clickStatus={clickActivated}
-                                            onClickChange={handleClickChange}
-                                            correctAnswer={correctAnswer}
-                                        />
-                                        {/* <Scoreboard playersInRoom={playersInRoom} /> */}
-                                    </div>
-                                ) : (
-                                    <EndGame players={players} player={player} />
-                                )
-                                }
-                            </div>
-                        )
-                        }
-                    </div>
-                )
-                }
-                {gameEnd === false ? (
-                   <ListOfPlayers playersInRoom={playersInRoom}/>
-                ) : (
-                    ""
-                )}
-
-
-            </div>
-
+                    )}
+                </>
+            )}
+            {gameStart && (
+                <>
+                    {!gameEnd && (
+                        <div>
+                            <h3>Time left: {timeLeft}</h3>
+                            <ProgressBar animated now={progress} label={`${timeLeft} seconds left`} />
+                            <GameQuestion
+                                currentQuestion={currentQuestion}
+                                currentOptions={currentOptions}
+                                currentRound={currentRound}
+                                playerName={playerName}
+                                socket={socket}
+                                clickStatus={clickActivated}
+                                onClickChange={handleClickChange}
+                                correctAnswer={correctAnswer}
+                            />
+                        </div>
+                    )}
+                    {gameEnd && (
+                        <EndGame players={players} player={player} />
+                    )}
+                </>
+            )}
+            {!gameEnd && (
+                <ListOfPlayers playersInRoom={playersInRoom} />
+            )}
         </Container>
     );
+
 };
 
 export default GamePlayer;
