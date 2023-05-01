b6c28e403203032a2a3949f0eceb810e347b26c3
marcovindy <72346394+marcovindy@users.noreply.github.com>
2023-05-01 14:46:14 +0100

    Delete files

--- server/index.js
+++ server/index.js
@@ -46,10 +46,6 @@ app.use(cors(corsOptions));
 const db = require("./models");
 
 // Routers
-const postRouter = require("./routes/Posts");
-app.use("/posts", postRouter);
-const commentsRouter = require("./routes/Comments");
-app.use("/comments", commentsRouter);
 const usersRouter = require("./routes/Users");
 app.use("/auth", usersRouter);
 const likesRouter = require("./routes/Likes");
